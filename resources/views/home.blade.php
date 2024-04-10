fdfd8fa41cc5429b6b30c5c3caf5bbab7297c6c3
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-04-10 16:33:11 +0200

    visualizazzione titolo in pagina

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,19 @@
     <h1>
         Lista Film
     </h1>
+
+    @dump($movies)
+    <ul>
+        @foreach ($movies as $movie)
+            <li>
+                <h1>
+                    {{$movie->title}}
+                </h1> 
+            </li>
+            
+        @endforeach
+    </ul>
+    
 </div>
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,1 +1,14 @@
-prova
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    @vite('resources/js/app.js')
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
